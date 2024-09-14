8bda20deded78116d493b154d5351c2de5374a93
sanji <2200591859@qq.com>
2024-09-14 17:11:19 +0800

    新增数据统计接口

--- src/api/largeScreen/largeScreen.js
+++ src/api/largeScreen/largeScreen.js
@@ -3,7 +3,7 @@ import request from '@/utils/request'
 //统计
 export function getCount(data) {
 	return request({
-		url: '/sys/visual/getCount',
+		url: '/api/web/visual/getCount',
 		method: 'post',
 		headers: {
 			"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
@@ -24,7 +24,29 @@ export function getCount(data) {
 //每日数据走势
 export function getCurve(data) {
 	return request({
-		url: '/sys/visual/getCurve',
+		url: '/api/web/visual/getCurve',
+		method: 'post',
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+		},
+		transformRequest: [
+			function(data) {
+				var ret = ''
+				for (var it in data) {
+					ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+				}
+				ret = ret.substring(0, ret.lastIndexOf('&'))
+				return ret
+			}
+		],
+		data
+	})
+}
+
+//七日充电趋势
+export function getSevenDayTrendByOrder(data) {
+	return request({
+		url: '/api/web/visual/getSevenDayTrendByOrder',
 		method: 'post',
 		headers: {
 			"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
@@ -46,7 +68,7 @@ export function getCurve(data) {
 //地图省份
 export function getProvinceByDevice(data) {
 	return request({
-		url: '/sys/visual/getProvinceByDevice',
+		url: '/api/web/visual/getProvinceByDevice',
 		method: 'post',
 		headers: {
 			"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
@@ -68,7 +90,7 @@ export function getProvinceByDevice(data) {
 //实时订单
 export function getOrderList(data) {
 	return request({
-		url: '/sys/visual/getOrderList',
+		url: '/api/web/visual/getOrderList',
 		method: 'post',
 		headers: {
 			"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
@@ -90,7 +112,7 @@ export function getOrderList(data) {
 //设备数据
 export function getDeviceCountAndDeviceFeeBack(data) {
 	return request({
-		url: '/sys/visual/getDeviceCountAndDeviceFeeBack',
+		url: '/api/web/visual/getDeviceCountAndDeviceFeeBack',
 		method: 'post',
 		headers: {
 			"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
@@ -112,7 +134,7 @@ export function getDeviceCountAndDeviceFeeBack(data) {
 //15天内无订单列表
 export function getOrderByGe15Day(data) {
 	return request({
-		url: '/sys/visual/getOrderByGe15Day',
+		url: '/api/web/visual/getOrderByGe15Day',
 		method: 'post',
 		headers: {
 			"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
@@ -134,7 +156,7 @@ export function getOrderByGe15Day(data) {
 //网点排名统计
 export function getListByNetWorkDot(data) {
 	return request({
-		url: '/sys/visual/getListByNetWorkDot',
+		url: '/api/web/visual/getListByNetWorkDot',
 		method: 'post',
 		headers: {
 			"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
@@ -157,7 +179,29 @@ export function getListByNetWorkDot(data) {
 //设备上下线日志
 export function getDeviceLogList(data) {
 	return request({
-		url: '/sys/visual/getDeviceLogList',
+		url: '/api/web/visual/getDeviceLogList',
+		method: 'post',
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+		},
+		transformRequest: [
+			function(data) {
+				var ret = ''
+				for (var it in data) {
+					ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+				}
+				ret = ret.substring(0, ret.lastIndexOf('&'))
+				return ret
+			}
+		],
+		data
+	})
+}
+
+//设备统计
+export function getDeviceCount(data) {
+	return request({
+		url: '/api/web/visual/getDeviceCount',
 		method: 'post',
 		headers: {
 			"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
