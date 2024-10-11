2fc43b0b973edfc025a37ffc9b4e0b5f5a94e277
sanji <2200591859@qq.com>
2024-10-11 22:43:13 +0800

    updated:充电趋势、站点趋势统计

--- src/api/business/businessStatistics.js
+++ src/api/business/businessStatistics.js
@@ -64,3 +64,91 @@ export function findDealerList(data) {
 		data
 	})
 }
+
+// 充电趋势详情
+export function chargingTrendList(data) {
+  return request({
+    url: '/api/web/reports/chargingTrend/list',
+    method: 'post',
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    },
+    transformRequest: [
+      function(data) {
+        var ret = ''
+        for (var it in data) {
+          ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+        }
+        ret = ret.substring(0, ret.lastIndexOf('&'))
+        return ret
+      }
+    ],
+    data
+  })
+}
+
+// 充电趋势曲线
+export function chargingTrend(data) {
+  return request({
+    url: '/api/web/reports/chargingTrend/statistics',
+    method: 'post',
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    },
+    transformRequest: [
+      function(data) {
+        var ret = ''
+        for (var it in data) {
+          ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+        }
+        ret = ret.substring(0, ret.lastIndexOf('&'))
+        return ret
+      }
+    ],
+    data
+  })
+}
+
+// 充电站趋势
+export function chargingStationSection(data) {
+  return request({
+    url: '/api/web/reports/chargingStationSection',
+    method: 'post',
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    },
+    transformRequest: [
+      function(data) {
+        var ret = ''
+        for (var it in data) {
+          ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+        }
+        ret = ret.substring(0, ret.lastIndexOf('&'))
+        return ret
+      }
+    ],
+    data
+  })
+}
+
+// 充电站趋势
+export function chargingStationSingle(data) {
+  return request({
+    url: '/api/web/reports/chargingStationSingle',
+    method: 'post',
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    },
+    transformRequest: [
+      function(data) {
+        var ret = ''
+        for (var it in data) {
+          ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+        }
+        ret = ret.substring(0, ret.lastIndexOf('&'))
+        return ret
+      }
+    ],
+    data
+  })
+}
