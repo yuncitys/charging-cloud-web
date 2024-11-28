832ee30fdaff86e54b0535b062b4600ce89050f3
sanji <2200591859@qq.com>
2024-11-28 21:30:39 +0800

    update:新增充电站用户消费资金明细

--- src/api/business/businessStatistics.js
+++ src/api/business/businessStatistics.js
@@ -174,3 +174,25 @@ export function chargingStationSingle(data) {
     data
   })
 }
+
+// 充电站趋势
+export function chargingStationFund(data) {
+  return request({
+    url: '/api/web/reports/chargingStationFund',
+    method: 'post',
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    },
+    transformRequest: [
+      function(data) {
+        var ret = ''
+        for (var it in data) {
+          ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+        }
+        ret = ret.substring(0, ret.lastIndexOf('&'))
+        return ret
+      }
+    ],
+    data
+  })
+}
