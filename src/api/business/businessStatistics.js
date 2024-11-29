e8f0d7a482e0e9f668b7cb758fc9614964452c43
sanji <2200591859@qq.com>
2024-11-29 23:26:46 +0800

    update:1、用户交易曲线统计  2、用户交易流水明细

--- src/api/business/businessStatistics.js
+++ src/api/business/businessStatistics.js
@@ -196,3 +196,48 @@ export function chargingStationFund(data) {
     data
   })
 }
+
+// 交易趋势
+export function chargingUserTradingCurve(data) {
+  return request({
+    url: '/api/web/reports/chargingUserTradingCurve',
+    method: 'post',
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    },
+    transformRequest: [
+      function(data) {
+        var ret = ''
+        for (var it in data) {
+          ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+        }
+        ret = ret.substring(0, ret.lastIndexOf('&'))
+        return ret
+      }
+    ],
+    data
+  })
+}
+
+// 交易趋势
+export function chargingUserTradingSection(data) {
+  return request({
+    url: '/api/web/reports/chargingUserTradingSection',
+    method: 'post',
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    },
+    transformRequest: [
+      function(data) {
+        var ret = ''
+        for (var it in data) {
+          ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+        }
+        ret = ret.substring(0, ret.lastIndexOf('&'))
+        return ret
+      }
+    ],
+    data
+  })
+}
+
