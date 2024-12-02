7e50868849c22c86b3569c6f70cd6699cb3df3e6
sanji <2200591859@qq.com>
2024-12-02 22:12:42 +0800

    update:用户充电汇总API

--- src/api/business/businessStatistics.js
+++ src/api/business/businessStatistics.js
@@ -265,9 +265,54 @@ export function chargingUserTradingSingle(data) {
 
 
 // 用户充电趋势
-export function chargingUserChargingSingle(data) {
+export function chargingUserChargeSingle(data) {
   return request({
-    url: '/api/web/reports/chargingUserChargingSingle',
+    url: '/api/web/reports/chargingUserChargeSingle',
+    method: 'post',
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    },
+    transformRequest: [
+      function(data) {
+        var ret = ''
+        for (var it in data) {
+          ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+        }
+        ret = ret.substring(0, ret.lastIndexOf('&'))
+        return ret
+      }
+    ],
+    data
+  })
+}
+
+
+// 用户充电趋势
+export function chargingUserChargeSection(data) {
+  return request({
+    url: '/api/web/reports/chargingUserChargeSection',
+    method: 'post',
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    },
+    transformRequest: [
+      function(data) {
+        var ret = ''
+        for (var it in data) {
+          ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+        }
+        ret = ret.substring(0, ret.lastIndexOf('&'))
+        return ret
+      }
+    ],
+    data
+  })
+}
+
+// 用户充电趋势
+export function chargingUserChargeCurve(data) {
+  return request({
+    url: '/api/web/reports/chargingUserChargeCurve',
     method: 'post',
     headers: {
       "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
