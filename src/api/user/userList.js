36c8ce820837300e375a2e7cc6926231e1a91f0e
sanji <2200591859@qq.com>
2025-01-08 18:46:46 +0800

    update：用户扣款API

--- src/api/user/userList.js
+++ src/api/user/userList.js
@@ -110,3 +110,25 @@ export function updateCash(data) {
 	})
 }
 
+//扣费
+export function updateBalance(data) {
+	return request({
+		url: '/api/web/appUser/updateBalance',
+		method: 'post',
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+		},
+		transformRequest: [
+			function(data) {
+				var ret = ''
+				for (var it in data) {
+					ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+				}
+				ret = ret.substring(0, ret.lastIndexOf('&'))
+				return ret
+			}
+		],
+		data
+	})
+}
+
