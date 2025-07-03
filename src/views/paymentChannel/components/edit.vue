50ed7261bd08b92aeae579f6dd4c0437174f4077
sanji <2200591859@qq.com>
2025-07-03 23:29:39 +0800

    feat:支付渠道

---
+++ src/views/paymentChannel/components/edit.vue
@@ -0,0 +1,437 @@
+<template>
+  <el-dialog :title="param.title" :visible.sync="propVisible" :before-close="close">
+    <el-form class="dialog-form" ref="form" :model="form" :rules="channelRule" :inline="true" label-width="auto">
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="渠道名称" prop="channelName">
+            <el-select v-model="form.channelName" clearable placeholder="支付方式">
+              <el-option v-for="(item,index) in channelList" 
+                :key="index" 
+                :label="item.name" 
+                :value="item.name"
+              ></el-option>
+            </el-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="服务提供商" prop="serviceProviderId">
+            <el-select v-model="form.serviceProviderId" clearable placeholder="请选择服务提供商">
+              <el-option v-for="(item,index) in serviceProviderList" 
+                :key="index" 
+                :label="item.name"
+                :value="item.code"
+              ></el-option>
+            </el-select>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="渠道账户" prop="channelAccount">
+            <el-input v-model="form.channelAccount" clearable placeholder="请输入渠道账户"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="渠道密码" prop="channelPassword">
+            <el-input show-password v-model="form.channelPassword" clearable placeholder="请输入渠道账户"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="账户负责人" prop="adminPeople">
+            <el-input v-model="form.adminPeople" placeholder="账户负责人" clearable></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <div class="box-config" v-if="form.channelName">
+        <el-divider><i class="el-icon-setting"></i>{{ form.channelName }}渠道配置</el-divider>
+        <div class="wxConfig" v-if="form.channelName === '微信'">
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="小程序id">
+                <el-input v-model="wxConfig.appletAccount.id" clearable placeholder="小程序id"></el-input>
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item label="小程序secret">
+                <el-input v-model="wxConfig.appletAccount.secret" clearable placeholder="小程序secret"></el-input>
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="公众号id">
+                <el-input v-model="wxConfig.officialAccount.id" clearable placeholder="公众号APPID"></el-input>
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item label="公众号secret">
+                <el-input v-model="wxConfig.officialAccount.secret" clearable placeholder="公众号secret"></el-input>
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="应用APPID">
+                <el-input v-model="wxConfig.appId" clearable placeholder="应用APPID"></el-input>
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item label="签名方式">
+                <el-input v-model="wxConfig.signType" clearable placeholder="签名方式"></el-input>
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="商户号">
+                <el-input v-model="wxConfig.mchId" clearable placeholder="商户号"></el-input>
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item label="API密钥">
+                <el-input v-model="wxConfig.key" clearable placeholder="API密钥"></el-input>
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="API版本">
+                <el-input v-model="wxConfig.apiVersion" clearable placeholder="API版本"></el-input>
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item label="API V3秘钥">
+                <el-input v-model="wxConfig.apiV3Key" clearable placeholder="API V3秘钥"></el-input>
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="证书序列号">
+                <el-input v-model="wxConfig.serialNo" clearable placeholder="序列号"></el-input>
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item label="回调域名">
+                <el-input v-model="wxConfig.callbackDomain" clearable placeholder="回调域名"></el-input>
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+          <el-col :span="8">
+            <el-form-item label="API证书文件">
+                <el-upload class="upload-demo" ref="uploadCertificateFile" :on-remove="handleRemoveCertificateFile" :file-list="certificateFileList"
+                  :http-request="uploadCertificateFile" action='' :on-change="handleChangeCertificateFile">
+                  <el-button slot="trigger" size="small" type="text" >选取.p12文件</el-button>
+                </el-upload>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="API私钥文件">
+              <el-upload class="upload-demo" ref="uploadCertificateKeyFile" :on-remove="handleRemoveCertificateKeyFile" :file-list="certificateKeyFileList"
+                :http-request="uploadCertificateKeyFile" action='' :on-change="handleChangeCertificateKeyFile">
+                <el-button slot="trigger" size="small" type="text">选取.pem文件</el-button>
+              </el-upload>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-button size="small" type="primary" @click="submitUpload()" :loading="loading">
+              上传文件
+            </el-button>
+          </el-col>
+        </el-row>
+        </div>
+        <div class="aliConfig" v-else-if="form.channelName === '支付宝'">
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="公众号id">
+                <el-input v-model="aliConfig.officialAccount.id" clearable placeholder="公众号APPID"></el-input>
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item label="公众号secret">
+                <el-input v-model="aliConfig.officialAccount.secret" clearable placeholder="公众号secret"></el-input>
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="应用APPID">
+                <el-input v-model="aliConfig.appId" clearable placeholder="应用APPID"></el-input>
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item label="pid">
+                <el-input v-model="aliConfig.pid" clearable placeholder="pid"></el-input>
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="私钥">
+                <el-input v-model="aliConfig.privateKey" clearable placeholder="私钥"></el-input>
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item label="alipayPublicKey">
+                <el-input v-model="aliConfig.alipayPublicKey" clearable placeholder="公钥"></el-input>
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="签名方式">
+                <el-input v-model="aliConfig.signType" clearable placeholder="签名方式"></el-input>
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item label="回调域名">
+                <el-input v-model="aliConfig.callbackDomain" clearable placeholder="回调域名"></el-input>
+              </el-form-item>
+            </el-col>
+          </el-row>
+        </div>
+      </div>
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="close">取 消</el-button>
+      <el-button type="primary" @click="confirm">确定</el-button>
+    </div>
+  </el-dialog>
+</template>
+
+<script>
+
+import { getInfo, update, save } from '@/api/channelConfigInfo'
+import { upload } from '@/api/upload/file.js'
+
+export default {
+  name: 'channelConfigInfo',
+  props: {
+    visible: {
+      type: Boolean,
+      default: false
+    },
+    param: {
+      type: Object,
+      default: () => {
+      }
+    }
+  },
+  computed: {
+    propVisible: {
+      get() {
+        return this.visible
+      },
+      set() {
+      }
+    }
+  },
+  data() {
+    return {
+      options: [],
+      form: {
+        'adminPeople': '',
+        'channelAccount': '',
+        'channelCode': '',
+        'channelName': '',
+        'channelPassword': '',
+        'configStr': '',
+        'serviceProviderId': ''
+      },
+      wxConfig: {
+        'officialAccount': { 'id': '', 'secret': '' },
+        'appletAccount': { 'id': '', 'secret': '' },
+        'appId': '',
+        'mchId': '',
+        'key': '',
+        'signType': '',
+        'apiVersion': null,
+        'apiV3Key': '',
+        'serialNo': '',
+        'cert': null,
+        'pemCert': '',
+        'callbackDomain': ''
+      },
+      aliConfig: {
+        'officialAccount': { 'id': '', 'secret': '' },
+        'sandbox': null,
+        'pid': '',
+        'appId': '',
+        'privateKey': '',
+        'alipayPublicKey': '',
+        'signType': '',
+        'useCert': null,
+        'appPublicCert': null,
+        'alipayPublicCert': null,
+        'alipayRootCert': null,
+        'callbackDomain': ''
+      },
+      channelList: [
+        { name: '微信', code: 'wxpay' },
+        { name: '支付宝', code: 'alipay' }
+      ],
+      serviceProviderList: [
+        { name: '微信', code: 'wxpay' },
+        { name: '支付宝', code: 'alipay' }
+      ],
+      channelRule: {
+        channelName: [
+          { required: true, message: '请选择支付方式', trigger: 'blur' }
+        ],
+        serviceProviderId: [
+          { required: true, message: '请选择渠道商', trigger: 'blur' }
+        ],
+        channelAccount: [
+          { required: true, message: '请输入渠道账户', trigger: 'blur' }
+        ],
+        channelPassword: [
+          { required: true, message: '请输入渠道密码', trigger: 'blur' }
+        ],
+        adminPeople: [
+          { required: true, message: '请输入账户负责人', trigger: 'blur' }
+        ]
+      },
+      loading: false,
+      fileList: [{
+        name: 'certificateFile',
+        file: null,
+      },{
+        name: 'certificateKeyFile',
+        file: null
+      }],
+      certificateFileList: [],
+      certificateKeyFileList: [],
+    }
+  },
+  created() {
+    this.init()
+    console.log("param:",this.param)
+    if (this.param.id) {
+      getInfo(this.param.id).then(res => {
+        this.form = res.data
+        if (this.form.channelName === '微信') {
+          this.wxConfig = JSON.parse(this.form.configStr)
+        } else if (this.form.channelName === '支付宝') {
+          this.aliConfig = JSON.parse(this.form.configStr)
+        }
+      })
+    }
+  },
+  methods: {
+    //选择文件
+    uploadCertificateFile(files) {
+      console.log(files)
+      this.fileList[0].file = files
+    },
+    //选择文件
+    uploadCertificateKeyFile(files) {
+      console.log(files)
+      this.fileList[1].file = files
+    },
+    //上传服务器
+    submitUpload() {
+      if (this.fileList[0].file == null) {
+        this.$message.error('请选择.p12文件')
+        return false
+      }
+      if (this.fileList[1].file == null){
+        this.$message.error('请选择.pem文件')
+        return false
+      }
+      this.loading = true
+      for (let i = 0; i < this.fileList.length; i++) {
+        let param = new FormData()
+        param.append('file', this.fileList[i].file.file)
+        console.log(param, "上传文件")
+        upload('PaymentFile',param).then(res => {
+          this.loading = false
+          if (res.code == 200) {
+            if (this.fileList[i].name == 'certificateFile') {
+              this.wxConfig.cert = res.data.url
+            }else{
+              this.wxConfig.pemCert = res.data.url
+            }
+            this.$message.success('上传成功')
+          } else {
+            this.$message.error(res.msg)
+          }
+        }).catch((err) => {
+          this.loading = false
+          this.$message.error('上传失败，请重试')
+        })
+      }
+    },
+    handleRemoveCertificateFile(file, certificateFileList) {
+      this.fileList[0].file = null
+    },
+    handleChangeCertificateFile(file, certificateFileList) {
+      if (certificateFileList.length > 1) {
+        certificateFileList.splice(0, 1);
+      }
+    },
+    handleRemoveCertificateKeyFile(file, certificateKeyFileList) {
+      this.fileList[1].file = null
+    },
+    handleChangeCertificateKeyFile(file, certificateKeyFileList) {
+      if (certificateKeyFileList.length > 1) {
+        certificateKeyFileList.splice(0, 1);
+      }
+    },
+    async init() {
+      
+    },
+    close() {
+      this.$emit('update:visible', false)
+    },
+    confirm() {
+      this.$refs.form.validate(valid => {
+        if (this.form.channelName === '微信') {
+          this.form.channelCode = 'wxpay'
+          this.form.configStr = JSON.stringify(this.wxConfig)
+        } else if (this.form.channelName === '支付宝') {
+          this.form.channelCode = 'alipay'
+          this.form.configStr = JSON.stringify(this.aliConfig)
+        }
+        if (valid) {
+          if (this.param.id) {
+            update(this.param.id, this.form).then(response => {
+              this.$message.success(response.msg)
+              this.$emit('search')
+              this.close()
+            })
+          } else {
+            save(this.form).then(response => {
+              this.$message.success(response.msg)
+              this.$emit('search')
+              this.close()
+            })
+          }
+        }
+      })
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+>>> .el-form {
+  .el-form-item {
+    display: flex;
+    width: 100%;
+    padding-right: 10px;
+
+    .el-form-item__label-wrap {
+      margin-left: 0;
+    }
+
+    .el-date-editor,
+    .el-select,
+    .el-form-item__content {
+      width: 100%;
+      flex: 1;
+    }
+  }
+}
+</style>
---
+++ src/views/paymentChannel/index.vue
@@ -0,0 +1,158 @@
+<template>
+  <div class="app-container">
+		<div class="filter-container">
+			<el-input v-model="searchForm.name" style="width: 150px;margin-right: 20px ;" class="filter-item"
+				placeholder="请输入渠道名称" clearable @keyup.enter.native="handleFilter" @clear="handleFilter()" />
+			<el-input v-model="searchForm.code" style="width: 150px;margin-right: 20px ;" class="filter-item"
+				placeholder="请输入渠道编号" clearable @keyup.enter.native="handleFilter" @clear="handleFilter()" />
+      <el-button type="primary" style="margin-right: 20px ;" class="filter-item" @click="handleFilter"
+				icon="el-icon-search">
+        查询
+      </el-button>
+
+      <el-button type="primary" style="margin-right: 20px ;" class="filter-item" icon="el-icon-plus" @click="add">
+        新增
+      </el-button>
+
+			<el-table v-loading="listLoading" :key="tableKey" :data="list" element-loading-text="拼命加载中......"  fithighlight-current-row style="width: 100%;" align="center" id="tableBox">
+        <el-table-column type="index" width="55" label="序号" align="center">
+          <template slot-scope="scope"><span>{{scope.$index+(page - 1) * limit + 1}} </span></template>
+        </el-table-column>
+        <el-table-column prop="channelName" label="渠道名称"></el-table-column>
+        <el-table-column prop="channelCode" label="渠道代码"></el-table-column>
+        <el-table-column prop="serviceProviderId" label="渠道服务商">
+          <template slot-scope="scope">
+						<span v-if="scope.row.serviceProviderId == 'wxpay'">微信</span>
+						<span v-if="scope.row.serviceProviderId == 'alipay'">支付宝</span>
+					</template>
+        </el-table-column>
+        <el-table-column prop="channelAccount" label="渠道账号"></el-table-column>
+        <el-table-column prop="channelPassword" label="渠道密码"></el-table-column>
+        <el-table-column prop="adminPeople" label="负责人"></el-table-column>
+        <el-table-column label="操作" width="150">
+            <template v-slot="{row}">
+                <el-tooltip class="item" effect="dark" content="修改" placement="top">
+                    <el-button type="primary" size="mini" icon="el-icon-edit" @click="edit(row)"></el-button>
+                </el-tooltip>
+
+                <el-tooltip class="item" effect="dark" content="删除本条数据" placement="top">
+                    <el-button @click="del(row)" icon="el-icon-delete"  size="mini" type="danger"></el-button>
+                </el-tooltip>
+            </template>
+        </el-table-column>
+      </el-table>
+
+      <div class="pagination-container">
+        <el-pagination :current-page="searchForm.page" :page-sizes="[10,20,30, 50]" :page-size="searchForm.limit"
+          :total="total" background layout="total, sizes, prev, pager, next, jumper"
+          @size-change="handleSizeChange" @current-change="handleCurrentChange" />
+      </div>
+
+      <!-- 修改 -->
+      <edit v-if="param.visible" :visible.sync="param.visible" :param="param" @search="search"></edit>
+		</div>
+	</div>
+</template>
+
+<script>
+import edit from './components/edit'
+import { getList, del, update } from '@/api/channelConfigInfo'
+export default {
+  name: 'channelConfigInfo', // "渠道信息"
+  components: { edit },
+  data () {
+    return {
+      page: 1,
+      limit: 10,
+      total: 10,
+      tableKey: 0,
+      listLoading: false,
+      list: [],
+      options: [{
+        value: '1',
+        label: '微信'
+      }, {
+        value: '0',
+        label: '支付宝'
+      }],
+      param: {
+        visible: false,
+        title: '新增',
+        id: ''
+      },
+      searchForm: {
+        page: 1,
+				limit: 10,
+        name: '',
+        code: '',
+        serviceProviderId: ''
+      }
+    }
+  },
+  created () {
+    this.search()
+  },
+  methods: {
+    handleSizeChange(val) {
+      this.searchForm.limit = val
+      this.getLists()
+    },
+    handleCurrentChange(val) {
+      this.searchForm.page = val
+      this.getLists()
+    },
+    handleFilter() {
+      this.searchForm.page = 1
+      this.getLists()
+    },
+    search () {
+      this.getLists()
+    },
+    add () {
+      this.param.title = '新增'
+      this.param.id = undefined
+      this.param.visible = true
+    },
+    edit (row) {
+      this.param.title = '编辑'
+      this.param.id = row.id
+      this.param.visible = true
+    },
+    updateStatus(e) {
+     update(e.id, {'enabledmark': e.enabledmark}).then(res => {
+        if(res.code == 200){
+          this.$message.success(res.msg)
+        }else{
+          this.$message.error(res.msg)
+        }
+      })
+    },
+    del (row) {
+      this.$confirm('这一操作将永久删除该记录。你想继续吗?', '警告', {
+        confirmButtonText: '是',
+        cancelButtonText: '否',
+        type: 'warning'
+      }).then(() => {
+        del(row.id).then(res => {
+          this.$message.success(res.msg)
+          this.search()
+        })
+      })
+    },
+    getLists() {
+      this.listLoading = true
+      let listQuery = JSON.parse(JSON.stringify(this.searchForm))
+      getList(listQuery).then(res => {
+        if (res.code == 200) {
+          console.log(res)
+          this.list = res.data
+          this.total = res.count
+          this.listLoading = false
+        } else {
+          this.$message.error(res.msg)
+        }
+      })
+    },
+  }
+}
+</script>
