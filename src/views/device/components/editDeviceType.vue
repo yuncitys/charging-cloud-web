f66940d3a41449296889342cc1a8b2a781bafc5b
sanji <2200591859@qq.com>
2025-08-15 23:45:24 +0800

    update:二维码前缀

--- src/views/device/components/editDeviceType.vue
+++ src/views/device/components/editDeviceType.vue
@@ -15,38 +15,41 @@
 						<el-radio :label="2">汽车</el-radio>
 					</el-radio-group>
 				</el-form-item>
-        <el-form-item :label="'电流输出'" prop="electricOut">
-        	<el-select v-model="formData.electricOut" placeholder="请选择电流输出类型" style="width: 100%;" @change="currElectricOutChange">
-        		<el-option v-for="item in electricOutList" :key="item.value" :label="item.label" :value="item.value"
-        			:disabled="item.disabled">
-        		</el-option>
-        	</el-select>
-        </el-form-item>
+				<el-form-item :label="'电流输出'" prop="electricOut">
+					<el-select v-model="formData.electricOut" placeholder="请选择电流输出类型" style="width: 100%;" @change="currElectricOutChange">
+						<el-option v-for="item in electricOutList" :key="item.value" :label="item.label" :value="item.value"
+							:disabled="item.disabled">
+						</el-option>
+					</el-select>
+				</el-form-item>
 				<el-form-item :label="'设备类型'" prop="deviceTypeId">
 					<el-select v-model="formData.deviceTypeId" style="margin-right: 20px ;width: 100%;"
 						class="filter-item" placeholder="请选择设备类型" clearable>
 						<el-option v-for="item in tags" :key="item.deviceTypeId" :label="item.deviceTypeName" :value="item.deviceTypeId">
-              <!--:disabled="showDeviceType"-->
-            </el-option>
+							<!--:disabled="showDeviceType"-->
+						</el-option>
 					</el-select>
 				</el-form-item>
-        <el-form-item label="收费类型" prop="deviceChagePattern" v-if="formData.ruleId === 1">
-        	<el-radio-group v-model="formData.deviceChagePattern" @change="changeChagePattern">
-        		<el-radio :label="0">时间</el-radio>
-        		<el-radio :label="1">电量</el-radio>
-        		<el-radio :label="2">功率</el-radio>
-        	</el-radio-group>
-        </el-form-item>
-        <el-form-item :label="'收费方案'" prop="devicePriceId">
-        	<el-select v-model="formData.devicePriceId" class="filter-item" placeholder="请选择收费方案"
-        		clearable style="width: 100%">
-        		<el-option v-for="item in priceTypeList" :key="item.id" :label="item.feeName"
-        			:value="item.id" />
-        	</el-select>
-        </el-form-item>
-        <el-form-item :label="'总功率/W'" prop="deviceTotalPower">
-        	<el-input v-model="formData.deviceTotalPower" clearable placeholder="请输入设备总功率" />
-        </el-form-item>
+				<!-- <el-form-item label="收费类型" prop="deviceChagePattern" v-if="formData.ruleId === 1">
+					<el-radio-group v-model="formData.deviceChagePattern" @change="changeChagePattern">
+						<el-radio :label="0">时间</el-radio>
+						<el-radio :label="1">电量</el-radio>
+						<el-radio :label="2">功率</el-radio>
+					</el-radio-group>
+				</el-form-item>
+				<el-form-item :label="'收费方案'" prop="devicePriceId">
+					<el-select v-model="formData.devicePriceId" class="filter-item" placeholder="请选择收费方案"
+						clearable style="width: 100%">
+						<el-option v-for="item in priceTypeList" :key="item.id" :label="item.feeName"
+							:value="item.id" />
+					</el-select>
+				</el-form-item> -->
+				<el-form-item :label="'二维码前缀'" prop="deviceQrLink">
+					<el-input v-model="formData.deviceQrLink" clearable placeholder="请输入设备二维码前缀" />
+				</el-form-item>
+				<el-form-item :label="'总功率/W'" prop="deviceTotalPower">
+					<el-input v-model="formData.deviceTotalPower" clearable placeholder="请输入设备总功率" />
+				</el-form-item>
 				<el-form-item>
 					<el-button type="primary" @click="confirm('formData')">确定</el-button>
 					<el-button @click="showDevice = false">取消</el-button>
@@ -62,7 +65,7 @@
 		findDeviceType,
 		updateDevice,
 		findDeviceInfoById,
-    findDevicePriceByPriceType
+    	findDevicePriceByPriceType
 	} from '@/api/device/deviceList.js'
 	export default {
 		props: {
@@ -74,33 +77,34 @@
 			}
 		},
 		data() {
-      let checkNum = (rule, value, callback) => {
-      	if (!value) {
-      		return new Error('必填信息')
-      	} else {
-      		let regx = /(^[1-9]\d*$)/;
-      		if (!regx.test(value)) {
-      			callback(new Error('请输入正整数'))
-      		} else {
-      			callback()
-      		}
-      	}
-      }
+			let checkNum = (rule, value, callback) => {
+				if (!value) {
+					return new Error('必填信息')
+				} else {
+					let regx = /(^[1-9]\d*$)/;
+					if (!regx.test(value)) {
+						callback(new Error('请输入正整数'))
+					} else {
+						callback()
+					}
+				}
+			}
 			return {
 				showDevice: false,
-        showDeviceType: true,
+        		showDeviceType: true,
 				formData: {
 					id: '',
 					deviceCode: '',
-          deviceTotalPower: '',
-          electricOut: '',
+					deviceTotalPower: '',
+					electricOut: '',
 					deviceTypeId: '',
-          deviceChagePattern: '',
-          devicePriceId: '',
-					ruleId: ''
+					deviceChagePattern: '',
+					devicePriceId: '',
+					ruleId: '',
+					deviceQrLink: ''
 				},
-        priceTypeList: [],
-        electricOutName: '',
+				priceTypeList: [],
+				electricOutName: '',
 				deviceRules: {
 					deviceCode: [{
 						required: true,
@@ -112,119 +116,125 @@
 						message: '请选择设备类型',
 						trigger: 'change'
 					}],
-          ruleId: [{
-          	required: true,
-          	message: '请选择归属系列',
-          	trigger: 'change'
-          }],
-          electricOut: [{
-          	required: true,
-          	message: '请选择电流输出类型',
-          	trigger: 'blur'
-          }],
-          devicePriceId: [{
-          	required: true,
-          	message: '请选择计费方案',
-          	trigger: 'blur'
-          }],
-          deviceChagePattern: [{
-          	required: true,
-          	message: '请选择计费类型',
-          	trigger: 'blur'
-          }],
-          deviceTotalPower: [{
-          	required: true,
-          	message: '请输入设备总功率',
-          	trigger: 'blur'
-          }, {
-          	validator: checkNum,
-          	trigger: 'blur'
-          }],
+					ruleId: [{
+						required: true,
+						message: '请选择归属系列',
+						trigger: 'change'
+					}],
+					electricOut: [{
+						required: true,
+						message: '请选择电流输出类型',
+						trigger: 'blur'
+					}],
+					devicePriceId: [{
+						required: true,
+						message: '请选择计费方案',
+						trigger: 'blur'
+					}],
+					deviceChagePattern: [{
+						required: true,
+						message: '请选择计费类型',
+						trigger: 'blur'
+					}],
+					deviceTotalPower: [{
+						required: true,
+						message: '请输入设备总功率',
+						trigger: 'blur'
+					}, {
+						validator: checkNum,
+						trigger: 'blur'
+					}],
+					deviceQrLink: [{
+						required: true,
+						message: '请输入二维码前缀',
+						trigger: 'blur'
+					}],
 				},
 				tags: [],
-        electricOutList:[
-          {
-            value:0,
-            label:'交流充电桩'
-          },
-          {
-            value:1,
-            label:'直流充电桩'
-          }
-        ]
+        		electricOutList:[
+					{
+						value:0,
+						label:'交流充电桩'
+					},
+					{
+						value:1,
+						label:'直流充电桩'
+					}
+				]
 			}
 		},
 		methods: {
 			onShowDevice() {
-        console.log(this.row_data)
+        		console.log(this.row_data)
 				this.showDevice = true
 				this.formData.deviceCode = this.row_data.deviceCode
 				this.formData.id = this.row_data.id
 				this.formData.ruleId = this.row_data.ruleId ?? 1
 				this.formData.deviceTypeId = this.row_data.deviceTypeId ? Number(this.row_data.deviceTypeId) : ''
-        this.formData.electricOut = this.row_data.electricOut !== '' ? Number(this.row_data.electricOut) :''
-        this.formData.deviceChagePattern = this.row_data.priceType !== '' ? Number(this.row_data.priceType) :''
-        this.formData.devicePriceId = this.row_data.devicePriceId !== '' ? Number(this.row_data.devicePriceId) :''
-        this.formData.deviceTotalPower = this.row_data.deviceTotalPower ? Number(this.row_data.deviceTotalPower) : ''
+				this.formData.electricOut = this.row_data.electricOut !== '' ? Number(this.row_data.electricOut) :''
+				this.formData.deviceChagePattern = this.row_data.priceType !== '' ? Number(this.row_data.priceType) :''
+				this.formData.devicePriceId = this.row_data.devicePriceId !== '' ? Number(this.row_data.devicePriceId) :''
+				this.formData.deviceTotalPower = this.row_data.deviceTotalPower ? Number(this.row_data.deviceTotalPower) : ''
+				this.formData.deviceQrLink = this.row_data.deviceQrcodeLink ? this.row_data.deviceQrcodeLink : ''
 				this.initElectricOutList(this.formData.ruleId)
 			},
-      //选择收费类型
-      changeChagePattern(e) {
-      	console.log(e)
-      	this.formData.deviceChagePattern = e
-        this.formData.devicePriceId = ''
-      	this.getDevicePriceByPriceType()
-      },
-      //获取方案列表
-      getDevicePriceByPriceType() {
-      	let ruleId = this.formData.ruleId
-      	let deviceChagePattern = this.formData.deviceChagePattern
-      	if (parseInt(deviceChagePattern) == 3) {
-      		deviceChagePattern = 2
-      	}
-      	if (this.formData.ruleId === 2) {
-      		deviceChagePattern = 1
-      	}
-      	let data = {
-      		priceType: deviceChagePattern,
-      		ruleId: ruleId
-      	}
-      	findDevicePriceByPriceType(data).then(res => {
-      		if (res.code == 200) {
-      			this.priceTypeList = res.data || []
-      		}
-      	})
-      },
-      ruleIdChange(ruleId){
-        this.formData.deviceTypeId = '',
-        this.formData.electricOut = '',
-        this.initElectricOutList(ruleId)
-      },
+			//选择收费类型
+			changeChagePattern(e) {
+				console.log(e)
+				this.formData.deviceChagePattern = e
+				this.formData.devicePriceId = ''
+				this.getDevicePriceByPriceType()
+			},
+			//获取方案列表
+			getDevicePriceByPriceType() {
+				let ruleId = this.formData.ruleId
+				let deviceChagePattern = this.formData.deviceChagePattern
+				if (parseInt(deviceChagePattern) == 3) {
+					deviceChagePattern = 2
+				}
+				if (this.formData.ruleId === 2) {
+					deviceChagePattern = 1
+				}
+				let data = {
+					priceType: deviceChagePattern,
+					ruleId: ruleId
+				}
+				findDevicePriceByPriceType(data).then(res => {
+					if (res.code == 200) {
+						this.priceTypeList = res.data || []
+					}
+				})
+			},
+			ruleIdChange(ruleId){
+				this.formData.deviceTypeId = '',
+				this.formData.electricOut = '',
+				this.initElectricOutList(ruleId)
+			},
 			initElectricOutList(ruleId) {
-        this.electricOutList = []
-			  if(ruleId === 1){
-			    this.electricOutList =
-			    [
-			      {
-			        value: 0,
-			        label: '交流充电桩'
-			      }
-			    ];
-			  }else if(ruleId === 2){
-			    this.electricOutList =
-			    [
-			      {
-			        value: 0,
-			        label: '交流充电桩'
-			      },
-			      {
-			        value: 1,
-			        label: '直流充电桩'
-			      },
-			    ];
-			  }
+        		this.electricOutList = []
+			  	if(ruleId === 1){
+					this.electricOutList =
+					[
+					{
+						value: 0,
+						label: '交流充电桩'
+					}
+					];
+			  	}else if(ruleId === 2){
+					this.electricOutList =
+					[
+					{
+						value: 0,
+						label: '交流充电桩'
+					},
+					{
+						value: 1,
+						label: '直流充电桩'
+					},
+					];
+			  	}
 				this.getDeviceTypeList()
-        this.getDevicePriceByPriceType()
+        		this.getDevicePriceByPriceType()
 			},
 			getDeviceTypeList() {
 				let ruleId = this.formData.ruleId
@@ -266,13 +276,13 @@
 			resetForm(formName) {
 				this.$refs[formName].resetFields();
 			},
-      //监听修改事件
-      currElectricOutChange(val){
-        console.log(val)
-        this.showDeviceType = false
-        this.formData.deviceTypeId = ''
-        this.getDeviceTypeList()
-      }
+      		//监听修改事件
+			currElectricOutChange(val){
+				console.log(val)
+				this.showDeviceType = false
+				this.formData.deviceTypeId = ''
+				this.getDeviceTypeList()
+			}
 		},
 		created() {
 		},
