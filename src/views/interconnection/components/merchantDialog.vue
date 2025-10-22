3ef2ae5c606f11073c60deee2debe3404404afd4
sanji <2200591859@qq.com>
2025-10-22 11:59:20 +0800

    update:fix bug id[23954]

--- src/views/interconnection/components/merchantDialog.vue
+++ src/views/interconnection/components/merchantDialog.vue
@@ -68,6 +68,14 @@
         }
 	  },
     data() {
+      let checkPhone = (rule, value, callback) => {
+        if (!(/^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$/.test(value))) {
+          callback(new Error('手机号码格式有误，请重填'))
+          return false;
+        } else {
+          callback()
+        }
+      };
       return {
         dialogVisible: false,
         rules: {
@@ -94,6 +102,9 @@
           contactInfo: [{
 						required: true,
 						message: '请输入联系方式',
+						trigger: 'blur'
+					}, {
+						validator: checkPhone,
 						trigger: 'blur'
 					}],
           interconnectionUrl: [{
