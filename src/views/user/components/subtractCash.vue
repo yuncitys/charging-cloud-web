2947fd095dd675955713f9327dd47c3cb839b76d
sanji <2200591859@qq.com>
2025-12-17 16:57:07 +0800

    update:fix bug id[24365]

--- src/views/user/components/subtractCash.vue
+++ src/views/user/components/subtractCash.vue
@@ -14,11 +14,11 @@
 					<el-input v-model="editData.cash" disabled />
 				</el-form-item>
 				<el-form-item :label="'充值金扣除金额'" prop="realityPayMoney">
-					<el-input v-model="realityPayMoney" placeholder="请输入充值金扣除金额" type="number" />
+					<el-input v-model="editData.realityPayMoney" placeholder="请输入充值金扣除金额" type="number" />
 				</el-form-item>
-        <el-form-item :label="'赠送金扣除金额'" prop="giveMoney">
-        	<el-input v-model="giveMoney" placeholder="请输入赠送金扣除金额" type="number" />
-        </el-form-item>
+				<el-form-item :label="'赠送金扣除金额'" prop="giveMoney">
+					<el-input v-model="editData.giveMoney" placeholder="请输入赠送金扣除金额" type="number" />
+				</el-form-item>
 				<el-form-item>
 					<el-button type="success" @click="updateBalance()">扣除</el-button>
 				</el-form-item>
@@ -46,17 +46,26 @@
 		},
 		data() {
 			return {
-				editData: {},
+				editData: {
+					userId: '',
+					phoneNumber: '',
+					cash: '',
+					realityPayMoney: '',
+					giveMoney: ''
+				},
 				updateMoneydialog: false,
-				realityPayMoney: '',
-        giveMoney: '',
 				remark: ''
 			}
 		},
 		methods: {
 			//显示充值弹窗
 			updateMoney() {
-				this.editData = this.row_data;
+				const data = this.row_data;
+				this.editData.userId = data.id;
+				this.editData.phoneNumber = data.phoneNumber;
+				this.editData.cash = data.cash;
+				this.editData.realityPayMoney = ''
+				this.editData.giveMoney = ''
 				this.updateMoneydialog = true
 			},
 			// 充值金额
@@ -67,22 +76,22 @@
 				// 	this.realityPayMoney = ''
 				// 	return false
 				// }
-				if (this.realityPayMoney == '') {
+				if (this.editData.realityPayMoney == '') {
 					this.$message.error("请输入扣除金额")
 					return
 				}
-        if (this.giveMoney == '') {
-        	this.$message.error("请输入扣除金额")
-        	return
-        }
+				if (this.editData.giveMoney == '') {
+					this.$message.error("请输入扣除金额")
+					return
+				}
 				this.$confirm('是否确认扣除?', '警告', {
 					confirmButtonText: '是',
 					cancelButtonText: '否',
 				}).then(() => {
 					let data = {
-						userId: this.editData.id,
-						realityPayMoney: this.realityPayMoney,
-						giveMoney: this.giveMoney
+						userId: this.editData.userId,
+						realityPayMoney: this.editData.realityPayMoney,
+						giveMoney: this.editData.giveMoney
 					}
 					console.log(data)
 					updateBalance(data).then(res => {
@@ -91,8 +100,6 @@
 								type: 'success',
 								message: res.msg
 							})
-							this.realityPayMoney = 0
-              				this.giveMoney = 0
 							this.updateMoneydialog = false
 							this.$emit('getLists')
 						} else {
