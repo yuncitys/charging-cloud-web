825550b51d7ee5ad2b22cc373e2cc9d93374b2ba
yuncitys <parking@yuncitys.com>
2026-01-16 16:03:32 +0800

    update：添加大屏地图组件地图下钻功能

--- src/common/chart.vue
+++ src/common/chart.vue
@@ -21,6 +21,10 @@
 				type: Array,
 				default: () => [],
 			},
+			mapName: {
+				type: String,
+				default: '全国'
+			}
 		},
 		computed: {
 			isDark() {
@@ -34,200 +38,208 @@
 		watch: {
 			cdata: {
 				handler(newData) {
-					if (this.isDark) {
-						this.options = {
-							showLegendSymbol: true,
-							tooltip: {
-								trigger: 'item',
-								textStyle: {
-									fontSize: 14,
-									lineHeight: 22,
-								},
-								position: point => {
-									return [point[0] + 50, point[1] - 20];
-								},
-								// 如果需要自定义 tooltip样式，需要使用formatter
-								/*
-								  formatter: params => {
-								    return `<div style=""> ... </div>`
-								  }
-								*/
-							},
-							visualMap: {
-								min: 0,
-								max: 10,
-								show: false,
-								seriesIndex: 0,
-								// 颜色
-								inRange: {
-									color: ['rgba(147, 168, 255, 1)', 'rgba(48, 78, 203, 1)'],
-								},
-							},
-							// 底部背景
-							geo: {
-								show: true,
-								aspectScale: 0.85, //长宽比
-								zoom: 1.2,
-								top: '10%',
-								left: '16%',
-								map: '全国',
-								roam: false,
-								itemStyle: {
-									normal: {
-										areaColor: 'rgba(0,0,0,0)',
-										shadowColor: 'rgba(7,114,204, .8)',
-										shadowOffsetX: 5,
-										shadowOffsetY: 5,
-									},
-									emphasis: {
-										areaColor: 'rgba(48, 78, 203, 1)',
-									},
-								},
-							},
-							series: [{
-								name: '设备数',
-								type: 'map',
-								aspectScale: 0.85, //长宽比
-								zoom: 1.2,
-								mapType: '全国', // 自定义扩展图表类型
-								top: '10%',
-								left: '16%',
-								itemStyle: {
-									normal: {
-										color: 'red',
-										areaColor: 'rgba(19,54,162, .5)',
-										borderColor: 'rgba(147, 168, 255, 1)',
-										borderWidth: 1,
-										shadowBlur: 7,
-										shadowColor: 'rgba(147, 168, 255, 1)',
-									},
-									emphasis: {
-										areaColor: '#4f7fff',
-										borderColor: 'rgba(48, 78, 203, 1)',
-										borderWidth: 2,
-										shadowBlur: 10,
-										shadowColor: 'rgba(48, 78, 203, 1)',
-									},
-								},
-								label: {
-									formatter: params => `${params.name}`,
-									show: true,
-									position: 'insideRight',
-									textStyle: {
-										fontSize: 14,
-										color: '#efefef',
-									},
-									emphasis: {
-										textStyle: {
-											color: '#fff',
-										},
-									},
-								},
-								data: newData,
-							}, ],
-						};
-					} else {
-						this.options = {
-							showLegendSymbol: true,
-							tooltip: {
-								trigger: 'item',
-								textStyle: {
-									fontSize: 14,
-									lineHeight: 22,
-								},
-								position: point => {
-									return [point[0] + 50, point[1] - 20];
-								},
-								// 如果需要自定义 tooltip样式，需要使用formatter
-								/*
-								  formatter: params => {
-								    return `<div style=""> ... </div>`
-								  }
-								*/
-							},
-							visualMap: {
-								min: 0,
-								max: 10,
-								show: false,
-								seriesIndex: 0,
-								// 颜色
-								inRange: {
-									color: ['rgba(41,166,206, .5)', 'rgba(69,117,245, .9)'],
-								},
-							},
-							// 底部背景
-							geo: {
-								show: true,
-								aspectScale: 0.85, //长宽比
-								zoom: 1.2,
-								top: '10%',
-								left: '16%',
-								map: '全国',
-								roam: false,
-								itemStyle: {
-									normal: {
-										areaColor: 'rgba(0,0,0,0)',
-										shadowColor: 'rgba(7,114,204, .8)',
-										shadowOffsetX: 5,
-										shadowOffsetY: 5,
-									},
-									emphasis: {
-										areaColor: '#00aeef',
-									},
-								},
-							},
-							series: [{
-								name: '设备数',
-								type: 'map',
-								aspectScale: 0.85, //长宽比
-								zoom: 1.2,
-								mapType: '全国', // 自定义扩展图表类型
-								top: '10%',
-								left: '16%',
-								itemStyle: {
-									normal: {
-										color: 'red',
-										areaColor: 'rgba(19,54,162, .5)',
-										borderColor: 'rgba(0,242,252,.3)',
-										borderWidth: 1,
-										shadowBlur: 7,
-										shadowColor: '#00f2fc',
-									},
-									emphasis: {
-										areaColor: '#4f7fff',
-										borderColor: 'rgba(0,242,252,.6)',
-										borderWidth: 2,
-										shadowBlur: 10,
-										shadowColor: '#00f2fc',
-									},
-								},
-								label: {
-									formatter: params => `${params.name}`,
-									show: true,
-									position: 'insideRight',
-									textStyle: {
-										fontSize: 14,
-										color: '#efefef',
-									},
-									emphasis: {
-										textStyle: {
-											color: '#fff',
-										},
-									},
-								},
-								data: newData,
-							}, ],
-						};
-					}
-					// 重新选择区域
-					this.handleMapRandomSelect();
+					this.updateOptions(newData);
 				},
 				immediate: true,
 				deep: true,
 			},
+			mapName() {
+				this.updateOptions(this.cdata);
+			}
 		},
 		methods: {
-			// 开启定时器
+			updateOptions(newData) {
+				if (this.isDark) {
+					this.options = {
+						showLegendSymbol: true,
+						tooltip: {
+							trigger: 'item',
+							textStyle: {
+								fontSize: 14,
+								lineHeight: 22,
+							},
+							position: point => {
+								return [point[0] + 50, point[1] - 20];
+							},
+							// 如果需要自定义 tooltip样式，需要使用formatter
+							/*
+							  formatter: params => {
+							    return `<div style=""> ... </div>`
+							  }
+							*/
+						},
+						visualMap: {
+							min: 0,
+							max: 10,
+							show: false,
+							seriesIndex: 0,
+							// 颜色
+							inRange: {
+								color: ['rgba(147, 168, 255, 1)', 'rgba(48, 78, 203, 1)'],
+							},
+						},
+						// 底部背景
+						geo: {
+							show: true,
+							aspectScale: 0.85, //长宽比
+							zoom: 1.2,
+							top: '10%',
+							left: '16%',
+							map: this.mapName,
+							roam: false,
+							itemStyle: {
+								normal: {
+									areaColor: 'rgba(0,0,0,0)',
+									shadowColor: 'rgba(7,114,204, .8)',
+									shadowOffsetX: 5,
+									shadowOffsetY: 5,
+								},
+								emphasis: {
+									areaColor: 'rgba(48, 78, 203, 1)',
+								},
+							},
+						},
+						series: [{
+							name: '设备数',
+							type: 'map',
+							aspectScale: 0.85, //长宽比
+							zoom: 1.2,
+							mapType: this.mapName, // 自定义扩展图表类型
+							top: '10%',
+							left: '16%',
+							itemStyle: {
+								normal: {
+									color: 'red',
+									areaColor: 'rgba(19,54,162, .5)',
+									borderColor: 'rgba(147, 168, 255, 1)',
+									borderWidth: 1,
+									shadowBlur: 7,
+									shadowColor: 'rgba(147, 168, 255, 1)',
+								},
+								emphasis: {
+									areaColor: '#4f7fff',
+									borderColor: 'rgba(48, 78, 203, 1)',
+									borderWidth: 2,
+									shadowBlur: 10,
+									shadowColor: 'rgba(48, 78, 203, 1)',
+								},
+							},
+							label: {
+								formatter: params => `${params.name}`,
+								show: true,
+								position: 'insideRight',
+								textStyle: {
+									fontSize: 14,
+									color: '#efefef',
+								},
+								emphasis: {
+									textStyle: {
+										color: '#fff',
+									},
+								},
+							},
+							data: newData,
+						}, ],
+					};
+				} else {
+					this.options = {
+						showLegendSymbol: true,
+						tooltip: {
+							trigger: 'item',
+							textStyle: {
+								fontSize: 14,
+								lineHeight: 22,
+							},
+							position: point => {
+								return [point[0] + 50, point[1] - 20];
+							},
+							// 如果需要自定义 tooltip样式，需要使用formatter
+							/*
+							  formatter: params => {
+							    return `<div style=""> ... </div>`
+							  }
+							*/
+						},
+						visualMap: {
+							min: 0,
+							max: 10,
+							show: false,
+							seriesIndex: 0,
+							// 颜色
+							inRange: {
+								color: ['rgba(41,166,206, .5)', 'rgba(69,117,245, .9)'],
+							},
+						},
+						// 底部背景
+						geo: {
+							show: true,
+							aspectScale: 0.85, //长宽比
+							zoom: 1.2,
+							top: '10%',
+							left: '16%',
+							map: this.mapName,
+							roam: false,
+							itemStyle: {
+								normal: {
+									areaColor: 'rgba(0,0,0,0)',
+									shadowColor: 'rgba(7,114,204, .8)',
+									shadowOffsetX: 5,
+									shadowOffsetY: 5,
+								},
+								emphasis: {
+									areaColor: '#00aeef',
+								},
+							},
+						},
+						series: [{
+							name: '设备数',
+							type: 'map',
+							aspectScale: 0.85, //长宽比
+							zoom: 1.2,
+							mapType: this.mapName, // 自定义扩展图表类型
+							top: '10%',
+							left: '16%',
+							itemStyle: {
+								normal: {
+									color: 'red',
+									areaColor: 'rgba(19,54,162, .5)',
+									borderColor: 'rgba(0,242,252,.3)',
+									borderWidth: 1,
+									shadowBlur: 7,
+									shadowColor: '#00f2fc',
+								},
+								emphasis: {
+									areaColor: '#4f7fff',
+									borderColor: 'rgba(0,242,252,.6)',
+									borderWidth: 2,
+									shadowBlur: 10,
+									shadowColor: '#00f2fc',
+								},
+							},
+							label: {
+								formatter: params => `${params.name}`,
+								show: true,
+								position: 'insideRight',
+								textStyle: {
+									fontSize: 14,
+									color: '#efefef',
+								},
+								emphasis: {
+									textStyle: {
+										color: '#fff',
+									},
+								},
+							},
+							data: newData,
+						}, ],
+					};
+				}
+				// 重新选择区域
+				this.handleMapRandomSelect();
+			},
+			/**
+			 * 开启定时器：用于随机高亮地图区域并展示提示
+			 */
 			startInterval() {
 				const _self = this;
 				// 应通过接口获取配置时间，暂时写死5s
@@ -239,7 +251,9 @@
 					_self.reSelectMapRandomArea();
 				}, time);
 			},
-			// 重新随机选中地图区域
+			/**
+			 * 重新随机选中地图区域：取消上次选中并随机选择新区域
+			 */
 			reSelectMapRandomArea() {
 				const length = this.cdata.length;
 				this.$nextTick(() => {
@@ -270,6 +284,12 @@
 					}
 				});
 			},
+			/**
+			 * 绑定地图交互事件并启动随机高亮：
+			 * - 鼠标移入时高亮当前区域并停止随机选择
+			 * - 鼠标移出时恢复随机选择
+			 * - 点击时向外部抛出事件（map-click）供父组件处理下钻
+			 */
 			handleMapRandomSelect() {
 				this.$nextTick(() => {
 					try {
@@ -278,6 +298,10 @@
 						setTimeout(() => {
 							_self.reSelectMapRandomArea();
 						}, 0);
+						map.off('click');
+						map.on('click', function(params) {
+							_self.$emit('map-click', params);
+						});
 						// 移入区域，清除定时器、取消之前选中并选中当前
 						map.on('mouseover', function(params) {
 							clearInterval(_self.intervalId);
--- src/components/Charts/mapChart.vue
+++ src/components/Charts/mapChart.vue
@@ -1,7 +1,17 @@
 <template>
-	<div>
-		<Chart :cdata="cdata" :style="{height:height,width:width}" />
-	</div>
+    <div>
+        <div class="map-breadcrumb">
+            <span
+                v-for="(item, idx) in breadcrumbs"
+                :key="idx"
+                class="crumb"
+                @click="goToLevel(idx)"
+            >
+                {{ item }}<span v-if="idx !== breadcrumbs.length - 1"> / </span>
+            </span>
+        </div>
+        <Chart :cdata="cdata" :mapName="mapName" @map-click="handleMapClick" :style="{height:height,width:width}" />
+    </div>
 </template>
 
 <script>
@@ -24,6 +34,8 @@
 		},
 		data() {
 			return {
+				mapName: '全国',
+                breadcrumbs: ['全国'],
 				cdata: [{
 						name: '北京市',
 						value: 0,
@@ -172,6 +184,77 @@
 			this.getProvinceByDevice()
 		},
 		methods: {
+            /**
+             * 处理地图点击事件：仅在“下钻成功”（存在已注册地图）时更新面包屑与地图
+             * - 全国 -> 省：只有当对应省份地图已注册时才切换并更新面包屑
+             * - 省 -> 市/区：同样仅在对应地图已注册时才切换；否则忽略点击，不追加面包屑
+             */
+            handleMapClick(params) {
+                if (!params || !params.name) return
+                if (this.mapName === '全国') {
+                    const targetFullName = params.name
+                    const targetMapName = this.toRegisteredMapName(targetFullName)
+                    if (this.isMapRegistered(targetMapName)) {
+                        this.mapName = targetMapName
+                        this.breadcrumbs = ['全国', targetFullName]
+                    }
+                } else {
+                    // 省级视图：尝试下钻城市，仅当已注册对应地图才允许
+                    const targetFullName = params.name
+                    const targetMapName = this.toRegisteredMapName(targetFullName)
+                    if (this.isMapRegistered(targetMapName)) {
+                        this.mapName = targetMapName
+                        // 省级面包屑第二项保持全称（如“福建省”），城市/区县使用点击名称
+                        const provinceFull = this.breadcrumbs[1]
+                        this.breadcrumbs = ['全国', provinceFull, targetFullName]
+                    }
+                }
+            },
+            /**
+             * 根据面包屑位置进行层级跳转
+             * idx 0：全国；idx 1：省份；>=2：城市/区县（保持省级地图，仅更新面包屑）
+             */
+            goToLevel(idx) {
+                if (idx === 0) {
+                    this.mapName = '全国'
+                    this.breadcrumbs = ['全国']
+                } else if (idx === 1) {
+                    const provinceFull = this.breadcrumbs[1]
+                    const provinceMap = this.toRegisteredMapName(provinceFull)
+                    if (this.isMapRegistered(provinceMap)) {
+                        this.mapName = provinceMap
+                        this.breadcrumbs = ['全国', provinceFull]
+                    }
+                } else if (idx >= 2) {
+                    // 城市/区县层：如果对应地图已注册则切换，否则保持当前地图，仅修剪面包屑
+                    const targetFull = this.breadcrumbs[idx]
+                    const targetMap = this.toRegisteredMapName(targetFull)
+                    if (this.isMapRegistered(targetMap)) {
+                        this.mapName = targetMap
+                    }
+                    this.breadcrumbs = this.breadcrumbs.slice(0, idx + 1)
+                }
+            },
+            /**
+             * 省份中文全称转为已注册的地图名称（与 echarts.registerMap 保持一致）
+             */
+            toRegisteredMapName(fullName) {
+                const map = {
+                    '福建省': '福建',
+                    '山东省': '山东'
+                }
+                return map[fullName] || fullName
+            },
+            /**
+             * 判断地图是否已通过 echarts.registerMap 注册
+             */
+            isMapRegistered(name) {
+                try {
+                    return !!this.$echarts && !!this.$echarts.getMap(name)
+                } catch (e) {
+                    return false
+                }
+            },
 			getProvinceByDevice() {
 				getProvinceByDevice().then(res => {
 					if (res.code === 200) {
@@ -197,4 +280,16 @@
 </script>
 
 <style lang="scss" scoped>
+    .map-breadcrumb {
+        font-size: 14px;
+        margin-bottom: 8px;
+        user-select: none;
+    }
+    .crumb {
+        cursor: pointer;
+        color: #409EFF;
+    }
+    .crumb:hover {
+        text-decoration: underline;
+    }
 </style>
