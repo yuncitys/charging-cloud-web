40c02acae2a59e6565373e80f645b176ab338bd7
sanji <2200591859@qq.com>
2026-02-10 15:42:01 +0800

    feat:api

---
+++ src/api/billRefundInfo/index.js
@@ -0,0 +1,41 @@
+import request from '@/utils/request'
+
+export function getList(data) {
+	return request({
+		url: '/api/payment/refundInfo/page',
+		method: 'POST',
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+		},
+		transformRequest: [
+			function(data) {
+				var ret = ''
+				for (var it in data) {
+					ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+				}
+				ret = ret.substring(0, ret.lastIndexOf('&'))
+				return ret
+			}
+		],
+		data
+	})
+}
+
+export function getInfo(id) {
+  return request({
+    url: `/api/payment/refundInfo/${id}`,
+    method: 'get'
+  })
+}
+
+export function del(id, data) {
+  return request({
+    url: `/api/payment/refundInfo/${id}`,
+    method: 'delete',
+    data: data
+  })
+}
+
+
+
+
