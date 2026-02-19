c5dada2372649b7ffb2f8208ab115c3de572d8f6
sanji <2200591859@qq.com>
2026-02-19 12:22:26 +0800

    update:商户站点接口查询

--- src/api/netWorkDot/netWorkDotList.js
+++ src/api/netWorkDot/netWorkDotList.js
@@ -111,3 +111,25 @@ export function getChargingStationList(data) {
 		data
 	})
 }
+
+//所有站点
+export function getChargeStationTreeByMerchant(data) {
+	return request({
+		url: '/api/web/networkDot/getChargeStationTreeByMerchant',
+		method: 'post',
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+		},
+		transformRequest: [
+			function(data) {
+				var ret = ''
+				for (var it in data) {
+					ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+				}
+				ret = ret.substring(0, ret.lastIndexOf('&'))
+				return ret
+			}
+		],
+		data
+	})
+}
