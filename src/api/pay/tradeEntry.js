91e984513706540dc734266f54c91b99dd7068cd
sanji <2200591859@qq.com>
2026-03-02 10:26:44 +0800

    fix(api): 修正交易商户接口URL路径错误
    更新交易商户相关API端点，将路径从`/tradeEntry`更正为`/tradeMerchant`以匹配后端路由
    同时修正图片识别接口路径，并新增商户状态查询功能

--- src/api/pay/tradeEntry.js
+++ src/api/pay/tradeEntry.js
@@ -3,7 +3,7 @@ import request from '@/utils/request'
 // 查询交易商户列表
 export function listTradeEntry(query) {
   return request({
-    url: '/api/pay/tradeEntry/list',
+    url: '/api/pay/tradeMerchant/list',
     method: 'get',
     params: query
   })
@@ -12,7 +12,7 @@ export function listTradeEntry(query) {
 // 查询交易商户详细
 export function getTradeEntry(id) {
   return request({
-    url: '/api/pay/tradeEntry/' + id,
+    url: '/api/pay/tradeMerchant/' + id,
     method: 'get'
   })
 }
@@ -20,7 +20,7 @@ export function getTradeEntry(id) {
 // 新增交易商户
 export function addTradeEntry(data) {
   return request({
-    url: '/api/pay/tradeEntry/saveTradeEntry',
+    url: '/api/pay/tradeMerchant/saveTradeMerchant',
     method: 'post',
     data: data
   })
@@ -29,7 +29,7 @@ export function addTradeEntry(data) {
 // 修改交易商户
 export function updateTradeEntry(data) {
   return request({
-    url: '/api/pay/tradeEntry/updateTradeEntry/' + data.id,
+    url: '/api/pay/tradeMerchant/updateTradeMerchant/' + data.id,
     method: 'put',
     data: data
   })
@@ -38,7 +38,7 @@ export function updateTradeEntry(data) {
 // 删除交易商户
 export function delTradeEntry(id) {
   return request({
-    url: '/api/pay/tradeEntry/' + id,
+    url: '/api/pay/tradeMerchant/' + id,
     method: 'delete'
   })
 }
@@ -46,7 +46,7 @@ export function delTradeEntry(id) {
 // 交易商户图片识别
 export function imgInfoDiscern(data) {
   return request({
-    url: '/imgInfoDiscern/platStream',
+    url: '/api/pay/merchantFile/imgInfoDiscern',
     method: 'post',
     data: data
     // request.js might not automatically set headers for FormData if not explicitly handled or if using a library like axios it usually does.
@@ -58,6 +58,14 @@ export function imgInfoDiscern(data) {
 export function getAreaSelector(id) {
   return request({
     url: `/api/permission/area/${id}/Selector`,
+    method: 'get'
+  })
+}
+
+// 交易商户状态查询
+export function queryTradeEntryStatus(id) {
+  return request({
+    url: `/api/pay/tradeMerchant/queryTradeEntry/${id}`,
     method: 'get'
   })
 }
