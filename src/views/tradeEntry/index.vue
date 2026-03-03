9e875dc302551753f77591c9eef147ec6be2261e
sanji <2200591859@qq.com>
2026-03-03 17:44:14 +0800

    feat(商户入驻): 新增商户列表查询筛选条件

--- src/views/tradeEntry/index.vue
+++ src/views/tradeEntry/index.vue
@@ -2,7 +2,28 @@
   <div class="app-container">
     <div class="filter-container">
       <el-input v-model="listQuery.merName" placeholder="商户名称" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />
-      <el-input v-model="listQuery.busTradeMerNo" placeholder="商户编号" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />
+      <el-input v-model="listQuery.busTradeMerNo" placeholder="业务方商户编号" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />
+      <el-input v-model="listQuery.merchantNo" placeholder="渠道商户号" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />
+      <el-input v-model="listQuery.serviceProviderId" placeholder="渠道代码" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />
+      <el-input v-model="listQuery.managerMobile" placeholder="管理员手机号" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />
+      <el-input v-model="listQuery.merCertNo" placeholder="证件号" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />
+      <el-select v-model="listQuery.status" placeholder="入驻状态" clearable class="filter-item" style="width: 130px">
+        <el-option v-for="(item, key) in statusMap" :key="key" :label="item" :value="key" />
+      </el-select>
+      <el-select v-model="listQuery.auditStatus" placeholder="审核状态" clearable class="filter-item" style="width: 130px">
+        <el-option v-for="(item, key) in auditStatusMap" :key="key" :label="item" :value="key" />
+      </el-select>
+      <el-date-picker
+        v-model="dateRange"
+        type="daterange"
+        range-separator="至"
+        start-placeholder="开始日期"
+        end-placeholder="结束日期"
+        value-format="yyyy-MM-dd"
+        class="filter-item"
+        style="width: 260px;"
+        @change="handleDateChange"
+      />
       <el-button class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">
         搜索
       </el-button>
@@ -139,11 +160,37 @@ export default {
       list: null,
       total: 0,
       listLoading: true,
+      dateRange: [],
+      statusMap: {
+        0: '待提交',
+        10: '入网中',
+        20: '认证中',
+        21: '待签署协议',
+        30: '正常',
+        31: '修改中',
+        32: '修改失败',
+        40: '冻结',
+        50: '注销',
+        60: '入网失败'
+      },
+      auditStatusMap: {
+        10: '待审核',
+        20: '审核拒绝',
+        30: '审核通过'
+      },
       listQuery: {
         page: 1,
         limit: 20,
         merName: undefined,
-        busTradeMerNo: undefined
+        busTradeMerNo: undefined,
+        merchantNo: undefined,
+        serviceProviderId: undefined,
+        managerMobile: undefined,
+        merCertNo: undefined,
+        status: undefined,
+        auditStatus: undefined,
+        createTimeStart: undefined,
+        createTimeEnd: undefined
       }
     }
   },
@@ -151,6 +198,15 @@ export default {
     this.getList()
   },
   methods: {
+    handleDateChange(val) {
+      if (val) {
+        this.listQuery.createTimeStart = val[0]
+        this.listQuery.createTimeEnd = val[1]
+      } else {
+        this.listQuery.createTimeStart = undefined
+        this.listQuery.createTimeEnd = undefined
+      }
+    },
     getList() {
       this.listLoading = true
       listTradeEntry(this.listQuery).then(response => {
@@ -204,3 +260,15 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.filter-container {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  margin-bottom: 12px;
+}
+.filter-item {
+  margin: 0 12px 12px 0 !important;
+}
+</style>
