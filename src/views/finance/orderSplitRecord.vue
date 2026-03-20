3d28bdb1b69e95da58a8ce842b7e672fed453fa9
sanji <2200591859@qq.com>
2026-03-20 12:08:33 +0800

    feat(财务): 调整分账记录页面的商户筛选和展示逻辑

--- src/views/finance/orderSplitRecord.vue
+++ src/views/finance/orderSplitRecord.vue
@@ -9,12 +9,12 @@
 				placeholder="请选择状态" clearable @change="handleFilter">
 				<el-option v-for="item in tags" :key="item.id" :label="item.title" :value="item.id" />
 			</el-select>
-			<el-select style="width: 200px;margin-right: 20px ;" class="filter-item" v-model="listQuery.adminId" filterable clearable @change="handleFilter()"
-				placeholder="请选择分账用户">
+			<el-select style="width: 200px;margin-right: 20px ;" class="filter-item" v-model="listQuery.merchantId" filterable clearable @change="handleFilter()"
+				placeholder="请选择商户">
 				<el-option
-					v-for="item in dealerList"
+					v-for="item in merchantList"
 					:key="item.id"
-					:label="item.adminFullname + '(' + item.adminName + ')'"
+					:label="item.name"
 					:value="item.id">
 				</el-option>
 			</el-select>
@@ -38,7 +38,7 @@
 				</el-table-column>
 				<el-table-column label="分账订单" prop="splitOrderCode" align="center" :show-overflow-tooltip='isPc'>
 				</el-table-column>
-				<el-table-column label="收款账号" prop="adminFullname" align="center" :show-overflow-tooltip='isPc'>
+				<el-table-column label="收款账号" prop="merchantName" align="center" :show-overflow-tooltip='isPc'>
 				</el-table-column>
 				<el-table-column label="分账金额(元)" prop="splitAmount" align="center" :show-overflow-tooltip='isPc'>
 				</el-table-column>
@@ -58,8 +58,8 @@
 				<el-table-column label="状态" prop="status" align="center" :show-overflow-tooltip="isPc">
 					<template slot-scope="scope">
             			<el-tag type="danger" v-if="scope.row.status == 'UNTREATED'">未处理</el-tag>
-						<el-tag v-if="scope.row.status == 'PROCESSING'">处理中</el-tag>
-						<el-tag type="success"v-if="scope.row.status == 'FINISH'">已完成</el-tag>
+						<el-tag type="danger" v-if="scope.row.status == 'PROCESSING'">处理中</el-tag>
+						<el-tag type="success" v-if="scope.row.status == 'FINISH'">已完成</el-tag>
 						<el-tag type="danger" v-if="scope.row.status == 'FAIL'">分账失败</el-tag>
 					</template>
 				</el-table-column>
@@ -79,6 +79,11 @@
 						<span>{{ scope.row.updateTime | formatDate }}</span>
 					</template>
 				</el-table-column>
+				<el-table-column label="操作" align="center" width="120" fixed="right">
+					<template slot-scope="scope">
+						<el-button type="primary" size="mini" @click="handleDetail(scope.row)">详情</el-button>
+					</template>
+				</el-table-column>
 			</el-table>
 			<div class="pagination-container">
 				<el-pagination :current-page="listQuery.page" :page-sizes="[10,20,30, 50]" :page-size="listQuery.limit"
@@ -95,8 +100,8 @@
 		getList,
 	} from '@/api/finance/orderSplitRecord.js'
 	import {
-		getAgent
-	} from '@/api/agent/agentList.js'
+		getMerchant
+	} from '@/api/merchant/merchant.js'
 	import {
 		parseTime
 	} from '@/utils/index'
@@ -117,7 +122,7 @@
 					page: 1,
 					limit: 10,
 					status: '',
-					adminId: '',
+					merchantId: 0,
 					orderCode: '',
 					splitOrderCode: '',
 					createTimeStart: '',
@@ -143,7 +148,7 @@
 				],
 
 				time: '',
-        		dealerList: [],
+        		merchantList: [],
 			}
 		},
 		filters: {
@@ -202,12 +207,27 @@
 			resetForm(formName) {
 				this.$refs[formName].resetFields();
 			},
-			findDealerList() {
-				getAgent().then(res => {
-					if (res.code == 200) {
-						this.dealerList = res.data
+			getMerchantList() {
+				getMerchant({ type: 1 }).then(res => {
+					if (res && res.code == 200) {
+						const list = Array.isArray(res.data) ? res.data : []
+						const hasPlatform = list.some(i => String(i.id) === '0')
+						this.merchantList = hasPlatform ? list : [{ id: 0, name: '平台商户' }].concat(list)
 					} else {
+						this.merchantList = []
 						this.$message.error(res.msg)
+					}
+				}).catch(() => {
+					this.merchantList = []
+				})
+			},
+			handleDetail(row) {
+				const id = row && (row.id || row.splitRecordId) ? (row.id || row.splitRecordId) : ''
+				if (id === null || id === undefined || id === '') return
+				this.$router.push({
+					name: 'orderSplitRecordDetail',
+					query: {
+						id
 					}
 				})
 			},
@@ -215,7 +235,7 @@
 		created() {
 			this.listQuery.splitOrderCode = this.$route.query.orderCode || ''
 			this.getList()
-			this.findDealerList()
+			this.getMerchantList()
 		},
 	}
 </script>
