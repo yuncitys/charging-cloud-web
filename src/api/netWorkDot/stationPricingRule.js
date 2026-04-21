db207b66fbe47d5d4a7fee6040943cb65ccdce8e
sanji <2200591859@qq.com>
2026-04-21 16:47:36 +0800

    feat: 新增获取车辆计价模板选项和详情的接口

--- src/api/netWorkDot/stationPricingRule.js
+++ src/api/netWorkDot/stationPricingRule.js
@@ -40,6 +40,39 @@ export function getStationCarPricingDetail(params) {
   })
 }
 
+function toFormUrlencodedRequestConfig(data, url) {
+  return {
+    url,
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+    },
+    transformRequest: [
+      function(data) {
+        var ret = ''
+        for (var it in data) {
+          ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+        }
+        ret = ret.substring(0, ret.lastIndexOf('&'))
+        return ret
+      }
+    ],
+    data
+  }
+}
+
+export function getCarPricingTemplateOptions(data) {
+  return request(toFormUrlencodedRequestConfig(data, '/api/web/price/findDevicePriceByPriceType'))
+}
+
+export function getCarPricingTemplateDetail(params) {
+  return request({
+    url: '/api/web/price/templateDetail',
+    method: 'get',
+    params
+  })
+}
+
 /** 按站点查询历史计费策略列表（归档记录） */
 export function getStationChargePriceHistoryList(params) {
   return request({
